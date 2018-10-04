c60477779012429647bf9fdbf0b228b09e40967a
Dan Rader <howdy@danrdesign.com>
2018-10-03 23:30:10 -0400

    home page basics

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,6 +27,10 @@ function showMouseOut() {
 // target the marquee with the class var-marquee,
 // give it the class active and make it visible
 
+
+
+// weird mousey magic for the about page
+
 $(document).ready(function() {
     var movementStrength = 20;
     var height = movementStrength / $(window).height();
@@ -35,8 +39,10 @@ $(document).ready(function() {
     $("#top-image").mousemove(function(e){
              var pageX = e.pageX - ($(window).width() / 2);
              var pageY = e.pageY - ($(window).height() / 2);
-             var newvalueX = width * pageX * 1 + 0;
-             var newvalueY = height * pageY * 1 + 0;
-             $('#top-image').css("background-position", "calc(100% + "+newvalueX+"px)"+" calc(50% + "+newvalueY+"px), "+" right center");
+             var newvalueX = width * pageX * 1 + 1;
+             var newvalueY = height * pageY * 1 + 1;
+             $('#top-image').css("background-position", "calc(100% + "+newvalueX+"px)"+" calc(50% + "+newvalueY+"px), "+"calc(100% + "+newvalueX+"px / 3)"+" calc(50% + "+newvalueY+"px / 3)" );
+             // $('#top-image').css("background-position", `calc(100% + ${newvalueX}px) calc(50% + ${newvalueY}), right center`);
+
     });
 });
